731c70cdd223de9c70df2a6ee231e4e6c736f1a3
subhasish Mandal <sm8017797063@gmail.com>
2020-10-22 13:36:15 +0530

    error detection

--- Startup.cs
+++ Startup.cs
@@ -76,6 +76,7 @@ namespace nCov1._0
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
